bbefd9804bbd77166d0564dcbb3cea068c63f21d
suga-ming <kby9901@naver.com>
2022-12-01 18:08:56 +0900

    Add

--- src/Todo/ToDoList.tsx
+++ src/Todo/ToDoList.tsx
@@ -1,104 +1,54 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useRecoilState } from "recoil";
+import { atom, useRecoilState } from "recoil";
 import { inputValueAtom } from "../atoms";
 
-// const ToDoList = () => {
-//   const [inputValue, setInputValue] = useRecoilState(inputValueAtom);
-//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = e;
-//     setInputValue(value);
-//   };
-//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     console.log(inputValue);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           onChange={onChange}
-//           value={inputValue}
-//           placeholder="여기에 입력하세요"
-//         ></input>
-//         <button>추가</button>
-//       </form>
-//     </div>
-//   );
-// };
+const toDoState = atom({
+  key: "toDo",
+  default: [],
+});
 
-interface IFormData {
-  errors: {
-    email: {
-      message: string;
-    };
-  };
-  email: string;
-  name: string;
-  password?: string;
-  password1?: string;
-  phoneNumber?: number;
-  extraError?: string;
+interface IToDo {
+  text: string;
+}
+
+interface IForm {
+  toDo: string;
 }
 
 const ToDoList = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IFormData>({ defaultValues: { email: "@naver.com" } });
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.password1) {
-      setError("password1", { message: "password가 다릅니다" });
-    }
-    setError(
-      "extraError",
-      { message: "server offline" },
-      { shouldFocus: true }
-    );
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onSubmit = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
-  console.log(errors);
+
   return (
-    <>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input
-          {...register("email", {
-            required: "email을 입력해주세요",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com",
-            },
-          })}
-          placeholder="여기에 입력하세요"
-        ></input>
-        <input
-          {...register("name", {
-            required: "name을 입력해주세요",
-            validate: (value) =>
-              !value.includes("by") ? "no by allowed" : true,
-            minLength: { value: 1, message: "1글자이상 입력해주세요" },
-          })}
-          placeholder="성함"
-        ></input>
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("password")}
-          placeholder="비밀번호를 입력해주세요"
-        ></input>
-        <input
-          {...register("password1")}
-          placeholder="비밀번호를 다시 입력해주세요"
-        ></input>
-        <input {...register("phoneNumber")} placeholder="폰번호"></input>
+    <div>
+      <h1>To Dos</h1>
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("toDo")} placeholder="여기에 입력하세요"></input>
         <button>추가</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
-    </>
+      <ul></ul>
+    </div>
   );
 };
+
+// interface IFormData {
+//   errors: {
+//     email: {
+//       message: string;
+//     };
+//   };
+//   email: string;
+//   name: string;
+//   password?: string;
+//   password1?: string;
+//   phoneNumber?: number;
+//   extraError?: string;
+// }
 
 export default ToDoList;
