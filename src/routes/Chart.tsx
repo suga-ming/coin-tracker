899ff150ebd8e8466333ee2cd9e32fa23a231985
suga-ming <kby9901@naver.com>
2023-04-03 17:12:36 +0900

    Design(Chart):label 색

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -52,39 +52,13 @@ const Chart = ({ coinId }: ChartProps) => {
               },
               categories: data?.map((price) => price.open),
               type: "datetime",
+              labels: {
+                style: {
+                  colors: "#fbc531",
+                },
+              },
             },
           }}
-          // options={{
-          //   theme: { mode: "dark" },
-          //   chart: {
-          //     height: 500,
-          //     width: 500,
-          //     toolbar: { show: false },
-          //     background: "transparent",
-          //   },
-          //   grid: { show: false },
-          //   stroke: { curve: "smooth", width: 5 },
-          //   yaxis: { show: false },
-          //   xaxis: {
-          //     axisBorder: { show: false },
-          //     labels: { show: false },
-          //     axisTicks: { show: false },
-          //     type: "datetime",
-          //     categories: data?.map((price) =>
-          //       new Date(price.time_close * 1000).toUTCString()
-          //     ),
-          //   },
-          //   fill: {
-          //     type: "gradient",
-          //     gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
-          //   },
-          //   colors: ["#0fbcf9"],
-          //   tooltip: {
-          //     y: {
-          //       formatter: (value) => `$ ${value.toFixed(2)}`,
-          //     },
-          //   },
-          // }}
           series={[
             {
               name: "Price",
